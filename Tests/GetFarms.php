cd97be7a46fbcd092ae5e3bac02fcf922c7c5899
crimsonronin <josh.stuart@zoopcommerce.com>
2013-05-09 07:03:43 +1000

    Update GetFarms.php

--- Tests/GetFarms.php
+++ Tests/GetFarms.php
@@ -6,8 +6,8 @@ require '../vendor/autoload.php';
 use Zoop\Scalr\Farm\FarmClient;
 
 $client = FarmClient::factory([
-            'key' => '417e5b524fd4268d',
-            'secret' => 'pteq00Suw3J/6/ZMJk6jFb2G9jBBruwxU41dnlANAmCVhWUrOoY9uvp1KBJ+Q1Utz7UMP5cbknNRGdLBlQUoIUvoIq4/AsRByfc/EGWmBo5yGc/AMVXfEYg0Czn60vnKpr29HfN6V+XXSdhgZfBBplQrBMSZkVW2aWJRK6oI3pk='
+            'key' => '',
+            'secret' => ''
         ]);
 
 $command = $client->getCommand('GetFarms');
